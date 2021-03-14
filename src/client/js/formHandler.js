7631d5b8338284317c0b9925d9a5349f9848c395
GoodnessObi <goodnessobi@gmail.com>
2021-03-14 14:32:28 +0100

    feature: make api call and update ui

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,13 +3,21 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    // Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8081/addData', {
+        method: 'POST',
+        body: { formText, isUrl }
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res)
+        let message = `${res.irony}, ${res.subjectivity}, ${res.agreement}, ${res.confidence}`
+        document.getElementById('results').innerHTML = message
+    })
+    .catch(error => {
+        console.log(error)
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,34 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+dotenv.config();
 
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
+
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
+const apiKey = process.env.API_KEY;
+let inputValue = 'http://tika.apache.org/1.3/formats.html'
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
 
 app.use(express.static('dist'))
 
 console.log(__dirname, '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
     res.sendFile(path.resolve('dist/index.html'))
 })
+
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
@@ -21,4 +37,18 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.post('/addData', async (req, res) => {
+    try{
+        const response = await fetch (`${baseURL}?key=${apiKey}&lang=auto&${req.body.isUrl ? 'url' : 'txt'}=${req.body.formText}`, {
+            method: 'POST'
+        })
+        const data = await response.json();
+        console.log(data);
+        console.log(data.agreement, data.subjectivity, data.irony)
+        res.send(data)
+    }catch(error) {
+        console.log(error, 'error')
+    }
 })
