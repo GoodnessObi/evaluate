9b27981bdfaf83471a24a40ac9c6ad994f5cc592
GoodnessObi <goodnessobi@gmail.com>
2021-03-23 20:40:38 +0100

    feature: set up unit testing

---
+++ _test_/clearUI.spec.js
@@ -0,0 +1,7 @@
+import { clearUI } from "../src/client/js/clearUI"
+   
+describe("Testing the clearUI functionality", () => {  
+  test("clearUI function exists", () => {
+    expect(clearUI).toBeDefined();
+  })
+});
---
+++ _test_/formHandler.spec.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler"
+   
+describe("Testing the handleSubmit functionality", () => {  
+  test("handleSubmit function exists", () => {
+    expect(handleSubmit).toBeDefined();
+  })
+});
